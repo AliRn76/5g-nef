241105478563e4d13ba6ca2db447283aca702821
Ali RajabNezhad <alirn76@yahoo.com>
2024-01-02 12:11:12 +0330

    nef created successfully :)

--- af.py
+++ af.py
@@ -4,7 +4,7 @@ from scapy.all import *
 def send_packet(packet):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("127.0.0.1", 8000))
+        s.connect(("127.0.0.1", 8001))
         s.send(bytes(packet[Raw]))
         print("Request Sent!")
     except Exception as e:
@@ -15,9 +15,9 @@ def read_pcap(path: str):
     scapy_cap = rdpcap(path)
     for packet in scapy_cap:
         if packet[IP].src == '10.100.233.89' and Raw in packet:
-            # breakpoint()
             send_packet(packet)
-            break  # TODO: For now
+            time.sleep(1)
+            # break  # TODO: For Test
 
 
 if __name__ == '__main__':
--- nef.py
+++ nef.py
@@ -1,37 +1,79 @@
-import random
+import json
 from collections import defaultdict
-from datetime import datetime
 
-import requests
+import httpx
 from pyDiameter.pyDiaMessage import DiaMessage
 
+FLOW_STATUSES = ['ENABLED-UPLINK', 'ENABLED-DOWNLINK', 'ENABLED', 'DISABLED', 'REMOVED']
+FLOW_USAGES = ['NO_INFO', 'RTCP', 'AF_SIGNALLING']
+MED_TYPE = ['AUDIO', 'VIDEO', 'DATA', 'APPLICATION', 'CONTROL', 'TEXT', 'MESSAGE', 'OTHER']
 
-def send_to_pcf():
-    n = random.randint(100, 999)
-    url = f'/nudr-dr/v1/policy-data/ues/imsi-999990000000{n}/sm-data?dnn=internet&snssai=%7B%0A%09%22sst%22%3A%091%0A%7D'
-    timestamp = datetime.utcnow().strftime('%a, %d %b %Y %I:%M:%S:%f GMT')
-    headers = {
-        ':authority': 'core5g-udr',
-        'accept': 'application/json,application/problem+json',
-        '3gpp-sbi-sender-timestamp': timestamp,
-        '3gpp-sbi-max-rsp-time': 10000,
-        'user-agent': 'PCF',
+UE_IP = '10.45.0.10'
+
+HTTP_CLIENT = httpx.Client(http1=False, http2=True)
+
+
+def send_to_pcf(data):
+    url = 'http://127.0.0.1:8000/npcf-policyauthorization/v1/app-sessions'
+
+    if 'Media-Component-Description' not in data:
+        print('Does not have "Media-Component-Description"')
+        return
+
+    codec = [c[:-1].decode() for c in data['Media-Component-Description']['Codec-Data AVP']]
+    media_component = data['Media-Component-Description']
+    payload = {
+        'ascReqData': {
+            'ueIpv4': UE_IP,
+            'afAppId': data['AF-Application-Identifier'].decode(),
+            'codecs': codec,
+            'ipv4Addr': '192.168.10.10',
+            'notifUri': 'https://google.com',
+            'suppFeat': '',
+            'medComponents': {
+                '0': {
+                    'medCompN': media_component['Media-Component-Number'],
+                    'marBwDl': str(media_component['Max-Requested-Bandwidth-DL']),
+                    'marBwUl': str(media_component['Max-Requested-Bandwidth-UL']),
+                    'fStatus': FLOW_STATUSES[media_component['Flow-Status']],
+                    'medType': MED_TYPE[media_component['Media-Type']],
+                    'medSubComps': {
+                        '0': {
+                            'fNum': media_component['Media-Sub-Component']['Flow-Number'],
+                            'flowUsage': FLOW_USAGES[media_component['Media-Sub-Component']['Flow-Usage']]
+                        }
+                    }
+                }
+            }
+
+        }
     }
+    # breakpoint()
 
-    response = requests.get(url=url, headers=headers)
-    print(response.status_code, response.content)
+    response = HTTP_CLIENT.post(url=url, json=payload)
+    if response.status_code == 201:
+        print(json.loads(response.text))
+    else:
+        print(f'Failed {response.content=}, {response.status_code=}')
 
 
 def parse_avp(avp):
-    avps = defaultdict(dict)
+    avp_dict = defaultdict(dict)
 
+    name = avp.getAVPName()
     value = avp.getAVPValue()
+
     if isinstance(value, list):
         for sub in value:
-            avps[avp.getAVPName()].update(parse_avp(sub))
+            sub_avp = parse_avp(sub)
+            for k, v in sub_avp.items():
+                if k in avp_dict[name]:
+                    avp_dict[name][k] = [avp_dict[name][k], v]
+                else:
+                    avp_dict[name][k] = v
     else:
-        avps[avp.getAVPName()] = value
-    return avps
+        avp_dict[name] = value
+    return avp_dict
 
 
 def parse_diameter(message):
@@ -48,7 +90,7 @@ def listen():
     import socket
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('127.0.0.1', 8000))
+    server.bind(('127.0.0.1', 8001))
     server.listen()
 
     while True:
@@ -57,8 +99,9 @@ def listen():
         avps = parse_diameter(message)
         communication_socket.close()
 
-        from pprint import pprint
-        pprint(avps)
+        print('\n-----------------------------------\n')
+        send_to_pcf(avps)
+        # pprint(avps)
 
 
 if __name__ == '__main__':
